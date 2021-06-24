80092fece4420ba53621c69be1c0ca17887af239
Lucas Pereira <lucas.pereira@canopusonline.com>
2021-06-23 23:07:12 -0300

    Ajustes e alteraçoes no calendario.

--- app/static/js/calendar.js
+++ app/static/js/calendar.js
@@ -1,8 +1,39 @@
-new HelloWeek({
+const picked = document.querySelector('#date-picked');
+
+const calendar = new HelloWeek({
     langFolder: "static/js/langs/",
     selector: '#calendar',
-    nav: ['⬅','➡'],
-    lang: 'pt',
+    nav: ['<','>'],
+    lang: 'pt-BR',
     disabledDaysOfWeek: [0],
     disablePastDays: true,
+    todayHighlight: false,
+    daysHighlight: [
+        {
+            days: ['2021-06-24'],
+            backgroundColor: '#5bc8ff',
+        },
+        {
+            days: ['2021-06-28'],
+            backgroundColor: '#f08080',
+        },
+        {
+            days: ['2021-06-30'],
+            backgroundColor: '#ffca59',
+        },
+        {
+            days: ['2021-07-22'],
+            backgroundColor: '#ffca59',
+        },
+        {
+            days: ['2021-07-17'],
+            backgroundColor: '#f08080',
+        },
+    ],
+    onSelect: () => {
+        document.getElementById('calendar').scrollIntoView();
+        picked.value = new Date(calendar.lastSelectedDay).toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+        // console.log(new Date(calendar.lastSelectedDay).toLocaleDateString('pt-BR', {timeZone: 'UTC'}));
+        // console.log(calendar.getDays()); //Forma com erro.
+    },
 });
